09434b65ee103c201936869f78f913fb1bb04ba1
Kosta Rashev <ni6to.bg@gmail.com>
2014-12-07 23:48:54 -0800

    Add start and quit game scripts

---
+++ Assets/MainMenu/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour 
+{
+	public void Quit(bool isDone)
+	{
+		if (isDone)
+		{
+			Application.Quit ();
+		}
+	}
+}
