c70c62e20dfea94633e9423d609e9eb3f513b540
Kosta Rashev <ni6to.bg@gmail.com>
2014-12-08 14:51:37 -0800

    Remove start and quit game scripts, add game manager script

---
+++ Assets/MainMenu/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	public void StartGame(int level)
+	{
+		Application.LoadLevel(level);
+	}
+
+	public void QuitGame(bool isDone)
+	{
+		if (isDone) 
+		{
+			Application.Quit();
+		}
+	}
+}
