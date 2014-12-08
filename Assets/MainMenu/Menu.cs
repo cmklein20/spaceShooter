5393fa166b25b0a2537536aea87a8bf7fc583893
Kosta Rashev <ni6to.bg@gmail.com>
2014-12-07 21:34:41 -0800

    Add menu and menu manager scripts

--- Assets/MainMenu/Menu.cs
+++ Assets/MainMenu/Menu.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Menu : MonoBehaviour {
+public class Menu : MonoBehaviour 
+{
+	private Animator animator;
+	private CanvasGroup canvasGroup;
 
-	// Use this for initialization
-	void Start () {
-	
+	public bool IsOpen
+	{
+		get { return animator.GetBool("IsOpen"); }
+		set { animator.SetBool("IsOpen", value); }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Awake()
+	{
+		animator = GetComponent<Animator>();
+		canvasGroup = GetComponent<CanvasGroup>();
+
+		var rect = GetComponent<RectTransform>();
+		rect.offsetMax = rect.offsetMin = new Vector2 (0, 0);
+	}
+
+	public void Update()
+	{
+		if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Open"))
+		{
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = false;
+		}
+		else
+		{
+			canvasGroup.blocksRaycasts = canvasGroup.interactable = true;
+		}
 	}
 }
