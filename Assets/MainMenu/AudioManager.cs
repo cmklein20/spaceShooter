8ba95d52c6232a42182c441abb7d6f5bbf5acad6
Kosta Rashev <ni6to.bg@gmail.com>
2014-12-08 18:02:54 -0800

    Add main menu buttons clicking effect

---
+++ Assets/MainMenu/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour 
+{
+	public AudioClip buttonClick;
+
+	private AudioSource source;
+
+	void Awake()
+	{
+		if (source == null)
+		{
+			source = GetComponent<AudioSource>();
+		}
+	}
+
+	public void OnMouseEnter()
+	{
+		if (source != null)
+		{
+			Debug.Log ("Source");
+		}
+
+		source.PlayOneShot(buttonClick);
+	}
+}
